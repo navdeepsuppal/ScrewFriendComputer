b324cbd900e7f7fca8bdf9122481b348bcc0e7a8
Navdeep Singh Uppal <navdeepuppal1609@gmail.com>
2021-03-18 22:04:34 +0530

    Window Box Added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import rotatescreen
 import time
+import ctypes
+
 screen = rotatescreen.get_primary_display()
 for i in range(10000):
-    time.sleep(1)
-    screen.rotate_to(i*90 % 360)
+     time.sleep(1)
+     screen.rotate_to(i*90 % 360)
+
+ctypes.windll.user32.MessageBoxW(0, "I Screwed your Box ! HAHAHA", "RIP", 1)
